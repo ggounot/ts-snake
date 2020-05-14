974d1bd1eb93d0b458efd48e9108bc81ac05d32f
Gérald <gerald@gounot.eu>
2020-05-14 06:16:21 +0200

    Set canvas dimensions

--- src/snake-canvas.ts
+++ src/snake-canvas.ts
@@ -1,9 +1,17 @@
+import settings from "./settings";
+import canvasSettings from "./canvas-settings";
+
 const main = (doc: Document = document) => {
   const element = doc.getElementById("canvas");
 
   if (element === null) {
     throw "Canvas element not found";
   }
+
+  const canvas = element as HTMLCanvasElement;
+
+  canvas.width = settings.gridWidth * canvasSettings.cellSize;
+  canvas.height = settings.gridHeight * canvasSettings.cellSize;
 };
 
 main();
