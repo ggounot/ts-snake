79059f27e3c6197a7e24619d0cebf75ad93948f8
Gérald <gerald@gounot.eu>
2020-05-14 07:00:39 +0200

    Draw initial state

--- src/snake-canvas.ts
+++ src/snake-canvas.ts
@@ -1,5 +1,26 @@
 import settings from "./settings";
 import canvasSettings from "./canvas-settings";
+import { initSnake } from "./snake";
+import { State } from "./types";
+
+const draw = (ctx: CanvasRenderingContext2D, state: State): void => {
+  ctx.fillStyle = canvasSettings.snakeColor;
+  state.snakePositions.forEach((position) =>
+    ctx.fillRect(
+      position.x * canvasSettings.cellSize,
+      position.y * canvasSettings.cellSize,
+      canvasSettings.cellSize,
+      canvasSettings.cellSize
+    )
+  );
+  ctx.fillStyle = canvasSettings.appleColor;
+  ctx.fillRect(
+    state.applePosition.x * canvasSettings.cellSize,
+    state.applePosition.y * canvasSettings.cellSize,
+    canvasSettings.cellSize,
+    canvasSettings.cellSize
+  );
+};
 
 const main = (doc: Document = document) => {
   const element = doc.getElementById("canvas");
@@ -12,6 +33,16 @@ const main = (doc: Document = document) => {
 
   canvas.width = settings.gridWidth * canvasSettings.cellSize;
   canvas.height = settings.gridHeight * canvasSettings.cellSize;
+
+  const ctx = canvas.getContext("2d");
+
+  if (ctx === null) {
+    throw "Could not get 2D rendering context";
+  }
+
+  const state = initSnake();
+
+  draw(ctx, state);
 };
 
 main();
