c0acd4294469109880ac3c86823237f8f4fdb5a3
Gérald <gerald@gounot.eu>
2020-05-14 06:14:27 +0200

    Add general settings

---
+++ src/settings.ts
@@ -0,0 +1,9 @@
+import { Direction } from "./types";
+
+export default {
+  gridWidth: 15,
+  gridHeight: 15,
+  defaultSnakePosition: { x: 5, y: 5 },
+  defaultSnakeDirection: Direction.Right,
+  stepsPerSecond: 6,
+};
