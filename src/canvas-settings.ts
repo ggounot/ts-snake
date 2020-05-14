59183d64e3f97cb81183f8e5df6efb96d1a08591
Gérald <gerald@gounot.eu>
2020-05-14 06:14:41 +0200

    Add canvas settings

---
+++ src/canvas-settings.ts
@@ -0,0 +1,9 @@
+export default {
+  cellSize: 20,
+  backgroundColor: "#1D1D1D",
+  snakeColor: "green",
+  appleColor: "red",
+  gameWonBackgroundColor: "rgba(0,255,0,0.01)",
+  gameLostBackgroundColor: "rgba(255,0,0,0.01)",
+  gameEndTextColor: "rgba(255,255,255,0.05)",
+};
